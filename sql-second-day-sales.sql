ace5c996689b7deb8f93ad4452286edc037279c8
max-student <max-student@maxtrain.com>
2020-01-30 13:34:11 -0500

    Sales Start

---
+++ sql-second-day-sales.sql
@@ -0,0 +1,14 @@
+Select sum(sales) as 'Total Sales for Cincy & Cle',
+	max(sales) as 'Highest Sales',
+	min(sales) as 'Lowest Sales',
+	avg(sales) as 'Average Sales',
+	count(*) as 'Number of Customers'
+from customers
+	where city = 'cincinnati' or city = 'cleveland'
+		--order by Sales DESC;
+
+Select sum(sales) as 'Total Sales', City
+	from customers
+		--where sales > 50000
+		group by city
+			having sum(sales) > 600000;
